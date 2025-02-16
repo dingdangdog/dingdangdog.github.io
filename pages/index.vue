f99d060bdaef5921765da1ee0c51166f38a3cbf9
dddx <dingdangdogx@outlook.com>
2025-02-16 21:39:32 +0800

    feat: update localization keys for products and tools, and adjust layout styles

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,13 @@
       </h1>
     </div>
     <!-- Introduction -->
-    <div class="p-2" style="max-width: 1080px; margin: auto">
+    <div class="p-2 max-w-5xl mx-auto">
       <p class="text-gray-500">{{ $t("motto") }}</p>
     </div>
     <!-- Products -->
-    <div class="bg-gray-800 rounded-md" style="max-width: 1080px; margin: auto">
+    <div class="bg-gray-800 rounded-md max-w-5xl mx-auto mt-4">
       <h2 class="text-lg md:text-xl text-center py-2 text-gray-300">
-        {{ $t("works.title") }}
+        {{ $t("products.title") }}
       </h2>
       <div class="flex flex-wrap justify-center md:p-2">
         <div
@@ -44,6 +44,39 @@
         </div>
       </div>
     </div>
+    <!-- <div class="bg-gray-800 rounded-md max-w-5xl mx-auto mt-4">
+      <h2 class="text-lg md:text-xl text-center py-2 text-gray-300">
+        {{ $t("tools.title") }}
+      </h2>
+      <div class="flex flex-wrap justify-center md:p-2">
+        <div
+          v-for="site in sites"
+          class="max-w-28 max-h-32 md:max-w-48 md:max-h-48 w-full h-full md:p-2 hover:p-0 m-1 md:m-0 duration-300 ease-in-out"
+        >
+          <div
+            class="w-full h-full rounded-md bg-gray-800 cursor-pointer hover:bg-gray-700 ease-in-out"
+            :title="site.url"
+          >
+            <a
+              class="w-full p-2 flex flex-col justify-center items-center"
+              :href="site.url"
+              target="_blank"
+            >
+              <img
+                :src="site.icon"
+                class="w-16 md:w-24 h-16 md:h-24 object-contain m-0 md:m-2 rounded-md overflow-hidden"
+              />
+              <h3
+                class="py-1 md:py-2 text-base md:text-xl"
+                :style="`color:${site.color}`"
+              >
+                {{ site.title }}
+              </h3>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -81,12 +114,12 @@ const zhSites: Site[] = [
     icon: "https://imgpro.oldmoon.top/favicon.png",
     color: "rgb(134,27,196)",
   },
-  {
-    title: "公众号MD编辑器",
-    url: "https://wxeditor.oldmoon.top",
-    icon: "https://wxeditor.oldmoon.top/assets/images/favicon.png",
-    color: "rgb(122,122,122)",
-  },
+  // {
+  //   title: "公众号MD编辑器",
+  //   url: "https://wxeditor.oldmoon.top",
+  //   icon: "https://wxeditor.oldmoon.top/assets/images/favicon.png",
+  //   color: "rgb(122,122,122)",
+  // },
 ];
 const enSites: Site[] = [
   {
@@ -113,12 +146,12 @@ const enSites: Site[] = [
     icon: "https://imgpro.oldmoon.top/favicon.png",
     color: "rgb(134,27,196)",
   },
-  {
-    title: "WeChat Editor",
-    url: "https://wxeditor.oldmoon.top",
-    icon: "https://wxeditor.oldmoon.top/assets/images/favicon.png",
-    color: "rgb(122,122,122)",
-  },
+  // {
+  //   title: "WeChat Editor",
+  //   url: "https://wxeditor.oldmoon.top",
+  //   icon: "https://wxeditor.oldmoon.top/assets/images/favicon.png",
+  //   color: "rgb(122,122,122)",
+  // },
 ];
 const { locale, locales } = useI18n();
 const sites = ref<Site[]>([]);
